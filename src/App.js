1bd738e8e542898f56b50ede00a716099445b95a
dariancmagee <dariancmagee@gmail.com>
2022-04-05 18:06:44 -0700

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { createContext } from 'react';
 import './App.css';
+import ClickMe from './components/clickMe';
+
+const moods = {
+  happy: '🦄',
+  sad: '👾',
+  setting: {},
+  theme: {},
+}
+
+export const MoodContext = createContext(moods)
 
 function App() {
   return (
+    <MoodContext.Provider value={moods.happy}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Hooks!</h1>
+      <ClickMe/>
     </div>
+    </MoodContext.Provider>
   );
 }
 
